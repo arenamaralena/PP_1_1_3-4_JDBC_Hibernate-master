d78762f55a9ecc35182e767af7c7d346af12b882
Arina M <arenamaralena@yandex.ru>
2025-04-25 13:45:55 +0300

    add Hibernate config

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,45 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/sys";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            settings.put("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            settings.put("hibernate.connection.url", URL);
+            settings.put("hibernate.connection.username", USERNAME);
+            settings.put("hibernate.connection.password", PASSWORD);
+            settings.put("hibernate.hbm2ddl.auto", "none");
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            System.err.println("Ошибка при создании SessionFactory.");
+        }
+    }
+
+    public static SessionFactory getSession() {
+        return sessionFactory;
+    }
 
     public Connection getConnection() {
         Connection connection = null;
@@ -25,5 +56,4 @@ public class Util {
         }
         return connection;
     }
-    // реализуйте настройку соеденения с БД
 }
